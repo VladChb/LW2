9a2a12ee1d92d5a72b28d259cbcb700045078740
VladuslavChabaniuk <chvladik07@gmail.com>
2025-09-23 09:49:01 +0300

    para oop

--- Lr2_1/Lr2_1.cpp
+++ Lr2_1/Lr2_1.cpp
@@ -3,24 +3,72 @@
 
 int main()
 {
-    setlocale(0, "ukr");
-    
-    Point p(1, 1);
-	cout << "Початкові координати точки: " << p.toStr() << endl;
+    setlocale(0, "ukr");  
 
-	p.opp();
-	cout << "Збільшенн координат на 1: " << p.toStr() << endl;
+    int n;
+    cout << "\nВведiть кiлькiсть точок: ";
+    cin >> n;
 
-	p.opp2();
-	cout << "Зменшенн координат на 1: " << p.toStr() << endl;
+    Point* points = new Point[n];
 
-	if(p.equal()) {
-		cout << "Координати рівні" << endl;
-	} else {
-		cout << "Координати не рівні" << endl;
-	}
-	Point d = p.addSc(2);
-	cout << "Додавання скаляра 2 до координат: " << d.toStr() << endl;
-	return 0;
+    for (int i = 0; i < n; i++) {
+        int x, y;
+        cout << "Введiть координати точки " << i + 1 << " (x y): ";
+        cin >> x >> y;
+        points[i] = Point(x, y);
+
+
+        Point p(x, y);
+        cout << "Початкові координати точки: " << p.toStr() << endl;
+
+        p.inc();
+        cout << "Збільшеня координат на 1: " << p.toStr() << endl;
+
+        p.dec();
+        cout << "Зменшеня координат на 1: " << p.toStr() << endl;
+
+        if (p.equal()) {
+            cout << "Координати рівні" << endl;
+        }
+        else {
+            cout << "Координати не рівні" << endl;
+        }
+
+    }
+
+    cout << "\nВведенi точки:\n";
+    for (int i = 0; i < n; i++) {
+        points[i].Print();
+        cout << "\nВiдстань до початку координат: "
+            << points[i].Calculate() << endl;
+    }
+
+    int a, b;
+    cout << "\nВведiть вектор зсуву (a b): ";
+    cin >> a >> b;
+
+    cout << "\nТочки пiсля перемiщення:\n";
+    for (int i = 0; i < n; i++) {
+        points[i].Move(a, b);
+        points[i].Print();
+        cout << "\n";
+    }
+
+    int k;
+    cout << "Введіть скаляр: ";
+    cin >> k;
+    if (n > 0) {
+        cout << "\nМножимо останню точку на: " << k << ":\n";
+        points[n - 1].setSk(k);
+        points[n - 1].Print();
+		cout << "\n";
+		Point p = points[n - 1];
+        Point d = p.addSc(k);
+        cout << "Додавання скаляра "<< k <<" до координат: " << d.toStr() << endl;
+    }
+
+    delete[] points;
+    return 0;
+
 }
 
--- Lr2_1/Point.h
+++ Lr2_1/Point.h
@@ -9,33 +9,36 @@ class Point
 	int y;
 
 public:
-	Point() : x(0), y(0) {};
-	Point(int x, int y) : x(x), y(y) {};
-	int getX();
-	int getY();
-	void setX(int val);
-	void setY(int val);
-	void Print();
-	double Calculate();
-	void Move(int a, int b);
-	void setSk(int k);
+    Point() : x(0), y(0) {}
+    Point(int x, int y) : x(x), y(y) {}
 
-	Point& opp() {
-		x++; y++;
-		return *this;
-	}
-	Point& opp2() {
-		x--; y--;
-		return *this;
-	}
 
-	bool equal() {
-		return x == y;
-	}
-	Point addSc(int scalar) {
-		return Point(x + scalar, y + scalar);
-	}
-	string toStr() {
-		return "X: " + to_string(x) + ", Y: " + to_string(y);
-	}
+    int getX();
+    int getY();
+    void setX(int val);
+    void setY(int val);
+    void Print();
+    double Calculate();
+    void Move(int a, int b);
+    void setSk(int k);
+    
+
+    void inc() { 
+        ++x; ++y; 
+    }
+    void dec() { 
+        --x; --y; 
+    }
+
+    bool equal() const { 
+        return x == y; 
+    }
+
+    Point addSc(int scalar) const {
+        return Point(x + scalar, y + scalar);
+    }
+
+    string toStr() const {
+        return "x=" + to_string(x) + ", y=" + to_string(y);
+    }
 };
